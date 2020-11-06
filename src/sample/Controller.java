f9b92f004c6568539e94134159c146f5d6c56907
liamdotcom <liam.vorster@ucdconnect.ie>
2020-11-06 16:47:36 +0200

    Grid Generator
    Added a menu function to generate the grid. To be improved further still.
    
    To do:
    -Generate maze
    -Generate special tiles

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,37 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.*;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+
 public class Controller {
+
+    @FXML
+    AnchorPane anchor;
+
+
+    public void changeColour(ActionEvent actionEvent) {
+        Rectangle rect=new Rectangle(0, 48, 20, 20);
+        rect.setFill(Color.WHITE);
+        rect.setStroke(Color.BLACK);
+        anchor.getChildren().add(rect);
+    }
+
+    public void generateGrid(ActionEvent actionEvent) {
+
+        for(int j=0;j<18;j++) {
+            for (int i = 0; i < 30; i++) {
+                Rectangle rect = new Rectangle(i * 20, (20*j) + 28, 20, 20);
+                rect.setFill(Color.WHITE);
+                rect.setStroke(Color.BLACK);
+                anchor.getChildren().add(rect);
+            }
+        }
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,8 +13,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.setScene(new Scene(root, 600, 390));
         primaryStage.show();
+
     }
 
 
