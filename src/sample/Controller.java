71fabbb7b02943445b3607f0c3fbe59dd97b2b93
liamdotcom <liam.vorster@ucdconnect.ie>
2020-11-07 19:24:18 +0200

    Basic grid generator implemented
    -Working on grid generation

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,8 @@
 package sample;
 
+import javafx.animation.FadeTransition;
+import javafx.animation.ParallelTransition;
+import javafx.animation.ScaleTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
@@ -7,31 +10,38 @@ import javafx.scene.paint.*;
 
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
 
 public class Controller {
 
     @FXML
     AnchorPane anchor;
-
-
-    public void changeColour(ActionEvent actionEvent) {
-        Rectangle rect=new Rectangle(0, 48, 20, 20);
-        rect.setFill(Color.WHITE);
-        rect.setStroke(Color.BLACK);
-        anchor.getChildren().add(rect);
-    }
+    Rectangle[][] grid=new Rectangle[18][30];
 
     public void generateGrid(ActionEvent actionEvent) {
-
         for(int j=0;j<18;j++) {
             for (int i = 0; i < 30; i++) {
                 Rectangle rect = new Rectangle(i * 20, (20*j) + 28, 20, 20);
                 rect.setFill(Color.WHITE);
                 rect.setStroke(Color.BLACK);
                 anchor.getChildren().add(rect);
+                grid[j][i]=rect;
+                var sctr = new ScaleTransition(Duration.millis(1000), grid[j][i]);
+                sctr.setByX(-0.2);
+                sctr.setByY(-0.2);
+                sctr.setCycleCount(2);
+                sctr.setAutoReverse(true);
+                var ptr=new ParallelTransition();
+                ptr.getChildren().add(sctr);
+                ptr.play();
             }
         }
 
+
+    }
+
+    public void clearGrid(){
+        grid[0][0].rem
     }
 }
